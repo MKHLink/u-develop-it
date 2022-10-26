ebe49e8d2ad73e6fbff9f9ed4d3693b107c444a5
Aleph Null <[likhonhasankomol@gmail.com]>
2022-10-26 01:02:24 -0400

    added voters table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 drop table if exists candidates;
 drop table if exists parties;
+drop table if exists voters;
 
 create table parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -14,5 +15,13 @@ CREATE TABLE candidates (
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
   constraint fk_party foreign key (party_id) references parties(id) on delete set null
+);
+
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at datetime default current_timestamp
 );
 
